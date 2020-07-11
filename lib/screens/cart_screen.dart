d85d6329fdc3cb1fd1d4ef685c0eeb234b4a42a1
pgolez <paul.golez@codingavenue.com>
2020-07-11 22:50:15 +0800

    Format layout for cart

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 
 class CartScreen extends StatelessWidget {
-
-static const ROUTE_NAME = '/cart';
+  static const ROUTE_NAME = '/cart';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,8 +12,35 @@ static const ROUTE_NAME = '/cart';
         children: <Widget>[
           Card(
             elevation: 6,
-            child: Row(
-              children: <Widget>[],
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: Row(
+                children: <Widget>[
+                  Text('Total Amount',
+                      style: const TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      )),
+                  Spacer(),
+                  Chip(
+                    label: Text(
+                      '0.0515',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    backgroundColor: Theme.of(context).primaryColor,
+                  ),
+                  FlatButton(
+                      onPressed: () {},
+                      child: Text(
+                        'ORDER NOW',
+                        style: TextStyle(
+                          color: Theme.of(context).primaryColor,
+                          fontSize: 14,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ))
+                ],
+              ),
             ),
           )
         ],
