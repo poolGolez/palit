b8f115f09735d066624812f22db66f8ff71e742b
pgolez <paul.golez@codingavenue.com>
2020-07-18 20:46:30 +0800

    Save orders to server

--- lib/providers/orders.dart
+++ lib/providers/orders.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
 
 import './order.dart';
 import 'cart.dart';
@@ -10,7 +13,9 @@ class Orders with ChangeNotifier {
 
   Order getAt(index) => _orders[index];
 
-  void addOrder(Cart cart) {
+  static const BASE_URL = 'https://palit-cbb04.firebaseio.com/orders';
+
+  Future<void> addOrder(Cart cart) async {
     final order = Order(
       DateTime.now().toString(),
       cart.items.fold({}, (map, entry) {
@@ -22,7 +27,24 @@ class Orders with ChangeNotifier {
       DateTime.now(),
     );
 
+    var requestBody = _createRequestBody(order);
+    await http.post("$BASE_URL.json", body: requestBody);
+
     _orders.add(order);
     notifyListeners();
+  }
+
+  String _createRequestBody(Order order) {
+    return json.encode({
+      'amount': order.amount,
+      'timestamp': order.dateTime.toIso8601String(),
+      'items': order.orderItems.entries.map((e) {
+        return {
+          'product_id': e.key.id,
+          'price': e.key.price,
+          'quantity': e.value,
+        };
+      }).toList()
+    });
   }
 }
